0ad9df641b965cfb5a8cd9e3fb04ddd87d71eb97
Burak <burakgomec101@gmail.com>
2021-06-16 23:10:08 +0300

    Add constants class

---
+++ PlayerMatcher_RestAPI/ConstantVariable/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PlayerMatcher_RestAPI.Constant
+{
+    public class Constants
+    {
+
+        public static string connectionInfo = "mongodb+srv://sunny:W2eSJGsKDB2mTHUn@cluster0.ldzet.mongodb.net/sample_airbnb.listingsAndReviews?authSource=admin";
+
+
+        private Constants(){}
+    }
+}
