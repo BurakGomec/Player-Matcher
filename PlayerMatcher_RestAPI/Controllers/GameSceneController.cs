2deb42110cb415daa50d1c711c07b3cea625ccb2
mert-erdem <47994087+mert-erdem@users.noreply.github.com>
2021-06-22 01:45:56 +0300

    Matchmaking system
    Player status checking and some minor fixes.

--- PlayerMatcher_RestAPI/Controllers/GameSceneController.cs
+++ PlayerMatcher_RestAPI/Controllers/GameSceneController.cs
@@ -40,26 +40,28 @@ namespace PlayerMatcher_RestAPI.Controllers
             //var filter = Builders<Player>.Filter.Empty;
             //var playerList = players.Find(filter).ToList();
 
-            var allDocuments = players.Find(new BsonDocument()).ToList();
+            var allPlayers = players.Find(new BsonDocument()).ToList();
 
+            var ids = new List<Guid>();
             var levels = new List<double>();
             var kdRatious = new List<double>();
-            #endregion
 
-            #region normalizasyon
-            foreach (var player in allDocuments)
+            foreach (var player in allPlayers)
             {
+                ids.Add(player.id);
                 levels.Add(player.level);
                 kdRatious.Add(player.kdRatio);
             }
+            #endregion
 
+            #region normalizasyon
             //listelerin min-max değerleri alınır
             double minLevel = levels.Min(), maxLevel = levels.Max(), minKD = kdRatious.Min(), maxKD = kdRatious.Max();
 
-            foreach (var player in allDocuments)
+            for (int i = 0; i < ids.Count; i++)
             {
-                player.level = (player.level - minLevel) / (maxLevel-minLevel);
-                player.kdRatio = (player.kdRatio - minKD) / (maxKD - minKD); 
+                levels[i] = (levels[i] - minLevel) / (maxLevel - minLevel);
+                kdRatious[i] = (kdRatious[i] - minKD) / (maxKD - minKD);
             }
             #endregion
 
@@ -67,21 +69,26 @@ namespace PlayerMatcher_RestAPI.Controllers
             //oyuncuların id leri ile öklid uzaklıkları hash lenir
             var euclideanDistances = new Dictionary<Guid, double>();
 
-            foreach (var player in allDocuments)
+            for (int i = 0; i < ids.Count; i++)
             {
-                if(player.id != player1.id)
+                if (ids[i] != player1.id)
                 {
                     //öklid uzaklığı bulunur
-                    double distance = Math.Sqrt(Math.Pow(player1.level - player.level, 2) + Math.Pow(player1.kdRatio - player.kdRatio, 2));
-                    euclideanDistances.Add(player.id, distance);
-                }                
+                    double distance = Math.Sqrt(Math.Pow(player1.level - levels[i], 2) + Math.Pow(player1.kdRatio - kdRatious[i], 2));
+                    var opponentCandidate = allPlayers.Find(x => x.id == ids[i]);
+
+                    if(opponentCandidate.status)//rakip adayı online ise
+                    {
+                        euclideanDistances.Add(ids[i], distance);
+                    }                   
+                }
             }
 
             //uzaklıklar azalan bir şekilde sıralanırlar
             euclideanDistances = euclideanDistances.OrderByDescending(x => x.Value).ToDictionary(y => y.Key, z => z.Value);
 
             //0. index de ki id ye sahip olan player2 bulunur
-            Player player2 = allDocuments.Find(x => x.id == euclideanDistances.ElementAt(0).Key);
+            Player player2 = allPlayers.Find(x => x.id == euclideanDistances.ElementAt(0).Key);
             #endregion
 
             return player2;
