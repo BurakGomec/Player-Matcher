ba230eb93f9b0608eac4af0608df8931113d5cc9
Burak <burakgomec101@gmail.com>
2021-06-25 00:20:35 +0300

    Add password variable to ignore files

--- PlayerMatcher_RestAPI/ConstantVariable/Constants.cs
+++ PlayerMatcher_RestAPI/ConstantVariable/Constants.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 namespace PlayerMatcher_RestAPI.Constant
 {
     public class Constants
     {
 
-        public static string connectionInfo = "mongodb+srv://sunny:W2eSJGsKDB2mTHUn@cluster0.ldzet.mongodb.net/sample_airbnb.listingsAndReviews?authSource=admin";
+        public static string connectionInfo = "";
 
 
         private Constants(){}
